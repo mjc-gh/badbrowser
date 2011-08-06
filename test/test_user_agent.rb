5ae116290b97d2408e83a5fdb1e72cebaee809fe
mikeycgto <mikeycgto@gmail.com>
2011-08-06 13:52:23 -0400

    add in a failure set to dynamic tests

--- test/test_user_agent.rb
+++ test/test_user_agent.rb
@@ -10,14 +10,20 @@ describe "AgentDetector" do
   
   # dynamic set of tests that use the user_agent fixture to generate themselves
   fixture(:user_agents).each do |browser, versions|
-    describe browser do
+    describe "#{versions.delete(:full_name) || browser}" do
       versions.each do |version, list|
         it "matches #{version}" do
           list.each do |agent|
             ua = user_agent(agent)
-           
-            ua.version.must_equal version
-            ua.send("#{browser}?").wont_be_nil
+            
+            if version.eql?(:invalid)
+              us.failed?.must_equal true
+            
+            else
+              ua.version.must_equal version
+              ua.send("#{browser}?").wont_be_nil
+              
+            end
           end          
         end        
       end
