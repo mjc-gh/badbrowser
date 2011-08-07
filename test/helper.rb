e1db4c2b3382c64f6ef4ba72b16d98fcd08ac6be
mikeycgto <mikeycgto@gmail.com>
2011-08-07 00:13:23 -0400

    update helper

--- test/helper.rb
+++ test/helper.rb
@@ -1,5 +1,6 @@
+gem "minitest"
 require 'minitest/autorun'
-require 'turn'
+require 'purdytest'
 require 'yaml'
 
 
@@ -12,6 +13,12 @@ def fixture(name)
   YAML.load( File.open(File.join('test', 'fixtures', "#{name}.yml"), 'r+').read )
 end
 
+def user_agent_fixtures
+  Hash[Dir['test/fixtures/user_agents/*.yml'].collect! { |fname|
+    [fname.match(/\w+\.yml/).to_s, YAML.load( File.open(fname, 'r+').read )]
+  }]
+end
+
 def user_agent(str) 
- AgentDetector.new(str)
+  AgentDetector.new(str)
 end
