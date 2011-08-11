9f9ae95cdcf20dd4a975060ebb3b3729655f7339
mikeycgto <mikeycgto@gmail.com>
2011-08-11 01:13:40 -0400

    Opera now works!

--- lib/user_agent.rb
+++ lib/user_agent.rb
@@ -12,9 +12,9 @@ class AgentDetector
   def initialize str
     @user_agent = str.to_s
     @user_agent.strip!
-    @failed = true
     
     detect_user_agent unless @user_agent.empty?
+    @failed = @version.nil?
   end
   
   def failed?
@@ -27,26 +27,37 @@ class AgentDetector
   def safari?; @safari; end
   def opera?; @opera; end
   
-  private 
+  protected 
   
   def detect_user_agent
-    if match_for(:msie, /MSIE (\d{1,2}\.[\dbB]{1,3})/) or
-       match_for(:chrome, /Chrome\/([\d{1,3}\.]+)/) or
-       match_for(:firefox, /Firefox[ \(\/]*([a-z0-9\.\-\+]*)/i) or
-       match_for(:opera, /Version\/(\d{1,2}\.\d{1,2})/) or
-       match_for(:opera, /Opera[ \(\/]*(\d{1,2}\.\d{1,2})/)
-      
-    else
-      @failed = true
-      
-    end
+    # "jump away" from for Opera (and Safari?) 
+    # String#include? is used here since it's quicker than RegEx at this point in detection
+    return parse_opera if @user_agent.include?('Opera')
+    
+    match_for(:msie, /MSIE (\d{1,2}\.[\dbB]{1,3})/) or 
+    match_for(:chrome, /Chrome\/([\d{1,3}\.]+)/) or
+    match_for(:firefox, /Firefox[ \(\/]*([a-z0-9\.\-\+]*)/i) 
   end
   
-  def match_for browser, regexp
-    match = @user_agent.match regexp
+  # We need a special method just for Opera since
+  # it's user agent strings are a nightmare (see fixtures)
+  def parse_opera
+    match = match_agent(/Version\/(\d{1,2}\.\d{1,2})/) || match_agent(/Opera[ \(\/]*(\d{1,2}\.\d{1,2}[u1]*)/)
+    
+    @opera = true
+    @version = match.last if match
+  end
+  
+  def match_for browser, regex
+    match = match_agent regex
     return false unless match
     
     instance_variable_set "@#{browser}", true
     @version = match.to_a.last
+  end
+  
+  def match_agent regex
+    match = @user_agent.match regex
+    match ? match.to_a : nil
   end
 end
