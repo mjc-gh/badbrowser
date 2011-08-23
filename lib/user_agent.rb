c940d6ddb857da90fc6e51c32b3a269e51eb9a8e
mikeycgto <mikeycgto@gmail.com>
2011-08-23 17:30:06 -0400

    pad the last digit on Opera via String#ljust. All Opera strings should have double digits on the minor version

--- lib/user_agent.rb
+++ lib/user_agent.rb
@@ -39,19 +39,26 @@ class UserAgent
   ##
   # We need a special method just for Opera since it's user agent strings are a nightmare (see fixtures).
   # Opera's user-agent include various tokens and strings that try to make it look like MSIE or Firefox
-  # Always set the browser to @opera since we know it is opera at this point
+  # Always set @opera as well since we know it is Opera at this point
   def parse_opera
     match = match_agent(/Version\/(\d{1,2}\.\d{1,2})/) || match_agent(/Opera[ \(\/]*(\d{1,2}\.\d{1,2}[u1]*)/)
-
-    @version = BrowserVersion.new match.last if match
-    @opera = true      
+    @opera = true
+    
+    if match
+      version = match.last.split('.')
+      version[1] = version[1].ljust(2, '0')
+      
+      @version = BrowserVersion.new version.join('.')
+    end
   end
   
+  ## 
   # version map for Safari 
-  @@safari_map = { :'2.0.4' => ['418.8', '419'], :'2.0.3' => ['417.9', '418'], :'2.0.2' => ['416.11', '416.12'], :'2.0.1' => ['412.7', '412.7'], 
-                   :'2.0' => ['412', '412.6.2'], :'1.3.2' => ['312.8', '312.8.1'], :'1.3.1' => ['312.5', '312.5.2'], :'1.3' => ['312.1', '312.1.1'], 
-                   :'1.2.4' => ['125.5.5', '125.5.7'], :'1.2.3' => ['125.4', '125.5'], :'1.2.2' => ['125.2', '125.2'], :'1.2' => ['124', '124'], 
-                   :'1.0.3' => ['85.8.2', '85.8.5'], :'1.0' => ['85.7', '85.7'] }
+  @@safari_map = {
+    :'2.0.4' => ['418.8', '419'], :'2.0.3' => ['417.9', '418'], :'2.0.2' => ['416.11', '416.12'], :'2.0.1' => ['412.7', '412.7'], :'2.0' => ['412', '412.6.2'], 
+    :'1.3.2' => ['312.8', '312.8.1'], :'1.3.1' => ['312.5', '312.5.2'], :'1.3' => ['312.1', '312.1.1'],  :'1.2.4' => ['125.5.5', '125.5.7'], 
+    :'1.2.3' => ['125.4', '125.5'], :'1.2.2' => ['125.2', '125.2'], :'1.2' => ['124', '124'],  :'1.0.3' => ['85.8.2', '85.8.5'], :'1.0' => ['85.7', '85.7'] 
+  }
 
   ##
   # We need a special method just to handle Safari since it's user agent strings are not
@@ -71,8 +78,7 @@ class UserAgent
       end
     end
   end
-  
-  ##
+    
   # Tries to match the user agent for the supplied browser via regex. The regex's are written so 
   # we at least can match the vendor. It's key this method returns a "trueish" value if any 
   # match is made (thus halting the OR chain in detect_user_agent)
@@ -82,7 +88,7 @@ class UserAgent
     @version = BrowserVersion.new match.to_a.last if match && match.size > 1
     instance_variable_set "@#{browser}", true if match
   end
-  
+    
   ##
   # Helper to make regex parsing a bit friendlier. It will convert the results to
   # an array and remove any nil or empty elements (or return nil if not match, the default behaviour)
