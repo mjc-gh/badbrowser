4bfa5d4e6efc944274347b7f2f0217b6c3638d22
mikeycgto <mikeycgto@gmail.com>
2011-08-25 14:36:12 -0400

    clean up app.rb a bit

--- app.rb
+++ app.rb
@@ -3,20 +3,16 @@ require 'sinatra/base'
 class BadBrowser < Sinatra::Base
   configure do
     set :logging, true
-    set :static, false # we'll use nginx to serve our page and such
-  end
-
-  helpers do
-    def user_agent agent
-      @user_agent ||= UserAgent.new agent
-    end
+    set :static, false
   end
 
   before do
-    user_agent request.user_agent
+    @user_agent ||= UserAgent.new agent
   end
 
   get "/detect.js" do
+    
+    
     content_type :js
     render :erb, :'result.js'
   end
