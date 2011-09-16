1ea4db6c8d1777d41033f09cb3ffb4359db4266a
mikeycgto <mikeycgto@gmail.com>
2011-09-16 12:19:49 -0400

    helpers and more code to API

--- lib/bad_browser.rb
+++ lib/bad_browser.rb
@@ -1,21 +1,37 @@
-require 'sinatra/base'
 require 'bad_browser/user_agent'
 
 class BadBrowser < Sinatra::Base
   configure do
+    use Rack::Logger
+    
     set :logging, true
     set :static, false
   end
 
+  helpers do
+    def browser; @user_agent.browser; end
+    def version; @user_agent.version; end
+    
+    def compare_versions
+      case @user_agent.browser
+      when :msie then version >= '7.0'
+      when :firefox then version >= '3.6.22'
+      when :chrome then version >= '4.1.249'
+      when :safari then version >= '4.0'
+      when :opera then version >= '9.0'
+      # else log it
+      end
+    end
+  end
+
   before do
     @user_agent ||= UserAgent.new request.user_agent
-    puts request.user_agent.inspect
-    puts @user_agent.inspect
   end
 
   get "/detect.js" do
     content_type :js
-    render :erb, :'detect.js'
+
+    !compare_versions ? '' : render(:haml, :'detect.js')
   end
   
   get "/" do # more for debug purposes
